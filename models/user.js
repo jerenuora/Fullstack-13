4370f1faebf84efb046fe409c6b075dfccf1478c
Jere Nuora <jere.nuora@gmail.com>
2022-11-28 21:05:48 +0200

    13.9 validation for correct email address- username and error messages for that

--- models/user.js
+++ models/user.js
@@ -4,26 +4,31 @@ const { sequelize } = require('../util/db')
 
 class User extends Model {}
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: true,
-  modelName: 'user'
-})
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: 'user',
+  }
+)
 
 module.exports = User

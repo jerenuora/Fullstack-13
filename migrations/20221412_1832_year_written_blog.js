129dbe3265e72323fd146bbb4a2ca84d2428ee5c
Jere Nuora <jere.nuora@gmail.com>
2022-12-14 18:43:53 +0200

    13.18 added years to blogs via migration

---
+++ migrations/20221412_1832_year_written_blog.js
@@ -0,0 +1,17 @@
+const { DATE } = require('sequelize')
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.addColumn('blogs', 'year', {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1991,
+        max: 2022
+      }
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.removeColumn('blogs', 'year')
+  },
+}
